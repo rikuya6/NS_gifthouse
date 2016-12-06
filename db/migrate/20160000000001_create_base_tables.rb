9e11b6ab4b1a41e656d461669f034f3ee065a036
rikua <rikua6@gmail.com>
2016-12-06 20:38:17 +0900

    マイグレーションファイル インデント修正

--- db/migrate/20160000000001_create_base_tables.rb
+++ db/migrate/20160000000001_create_base_tables.rb
@@ -3,7 +3,7 @@ class CreateBaseTables < ActiveRecord::Migration
     # ユーザ
     create_table :users do |t|
       t.string  :email,            null: false
-      t.boolean :administrator,   null: false, default: false
+      t.boolean :administrator,    null: false, default: false
       t.string  :password_digest,  null: false
 
       t.timestamps null: false
